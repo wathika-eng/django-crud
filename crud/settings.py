70c3bd2e7f0bb030aa7b5ebbb99dfacf6a955e37
wathika <71040609+wathika-eng@users.noreply.github.com>
2023-11-17 16:17:04 +0300

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['https://djangocrud.azurewebsites.net/'] #os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
